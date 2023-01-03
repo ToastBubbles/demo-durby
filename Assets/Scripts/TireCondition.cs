9211c5590b8cdc204f6e6c65c7d65582ad1d3cf3
ToastBubbles <jeffneal11@gmail.com>
2023-01-02 19:42:52 -0600

    tires can now be popped

--- Assets/Scripts/TireCondition.cs
+++ Assets/Scripts/TireCondition.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class TireCondition : MonoBehaviour
 {
     public bool isFlat = false;
+    private Vector3 originalSize;
     void Start()
     {
-
+        originalSize = gameObject.transform.localScale;
     }
 
 
     void Update()
     {
-
+        if (isFlat)
+        {
+            gameObject.transform.localScale = new Vector3(originalSize.x * 0.01f, originalSize.y * 0.01f, originalSize.z * 0.01f);
+        }
     }
 
     public void MakeFlat()
